64ea750cbef62bebca69b9e3f4f3c3fd642aa1b5
feb <lilele227@163.com>
2018-12-14 15:16:24 +0800

    city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,6 +2,7 @@
 	<div>
 		<city-header></city-header>
  		<city-search></city-search>
+ 		<city-list></city-list>
 	</div>
 
 
@@ -10,11 +11,13 @@
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
 export default {
   name: 'City',
   components: {
   	CityHeader,
-  	CitySearch
+  	CitySearch,
+  	CityList
   }
 }
 </script>
