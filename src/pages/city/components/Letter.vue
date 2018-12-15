ad1763c7f284e3f08a3717d167c25c5154ba09a0
feb <lilele227@163.com>
2018-12-15 13:51:17 +0800

    city-components

--- src/pages/city/components/Letter.vue
+++ src/pages/city/components/Letter.vue
@@ -1,7 +1,16 @@
 <template>
   <ul class="list">
-    <li class="item" v-for='item in cityList' @click='letterClick'>
-    {{item.initial}}
+    <li 
+      class="item" 
+      v-for='item in letters'
+      :key='item'
+      :ref='item'
+      @click='letterClick' 
+      @touchstart='handleTouchStart'
+      @touchmove='handleTouchMove'
+      @touchend='handleTouchEnd'
+    >
+    {{item}}
     </li>
   </ul>
 </template>
@@ -12,11 +21,44 @@ export default {
   props: {
     cityList: Array
     },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for(let i=0;i<this.cityList.length;i++) {
+        letters.push(this.cityList[i].initial)
+      }
+      return letters
+    }
+  },
   methods: {
     letterClick (e) {
       this.$emit('change',e.target.innerText)
+
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if(this.touchStatus){
+        const startY = this.$refs['A'][0].offsetTop
+        const endY = e.changedTouches[0].clientY - 79
+        const touchRange = endY-startY
+        const index = Math.floor(touchRange/19)
+        if(index >= 0 && index < this.letters.length){
+          this.$emit('change',this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
     }
-  }
+  },
+
 }
 </script>
 
